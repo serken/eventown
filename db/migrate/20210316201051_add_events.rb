9f0cf7431e5dcaf1ea0232c5abb75287581cbad3
Pavel Kharchenko <Pavel_Kharchenko@comcast.com>
2021-04-02 12:49:04 +0300

    Update migration

--- db/migrate/20210316201051_add_events.rb
+++ db/migrate/20210316201051_add_events.rb
@@ -2,14 +2,14 @@ class AddEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :title, null: false, default: ""
-      t.string :description, null: false, default: ""
+      t.string :description
       t.integer :event_type, null: false, default: Event.event_types[:party]
-      t.string :image, null: false, default: ""
-      t.float :cost, null: false, default: ''
-      t.string :remote_url, null: false, default: ''
+      t.string :image
+      t.float :cost
+      t.string :remote_url
       t.datetime :start_date
-      t.string :location, default: '', null: false
-      t.string :phone_number, null: false, default: false
+      t.string :location
+      t.string :phone_number
 
       t.timestamps null: false
     end
