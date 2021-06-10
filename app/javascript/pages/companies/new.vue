6fd19ebfa08cb44b08553f499ded58251060f208
zagana <71128760+zagana@users.noreply.github.com>
2021-06-10 21:35:10 +0300

    Update new.vue

--- app/javascript/pages/companies/new.vue
+++ app/javascript/pages/companies/new.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
       <v-card-title>
-        <span class="headline">Новая организация</span>
+        <span class="headline">Создать организацию</span>
       </v-card-title>
       <v-card-text>
         <v-container>
